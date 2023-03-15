8d4988bf683293018b0bb1a47925193c8487e404
Giuseppe Pio Maffia <43602245+Gmaffia596@users.noreply.github.com>
2023-03-15 13:45:11 +0100

    aggiunta slidershow

--- sottosopra3c/lib/home_screen.dart
+++ sottosopra3c/lib/home_screen.dart
@@ -1,11 +1,37 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ImageSlideshow(
+      onPageChanged: (value) {
+        debugPrint('Page changed: $value');
+      },
+      autoPlayInterval: 3000,
+      isLoop: true,
+      children: [
+        Image.asset(
+          'lib/assets/1.jpg',
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          'lib/assets/2.jpg',
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          'lib/assets/3.jpg',
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          'lib/assets/4.jpg',
+          fit: BoxFit.cover,
+        ),
+      ],
+    );
   }
 }
